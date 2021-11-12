f2fd76952a5aa32c8019c5d883abd4874b2ce234
Prajwalgn-07 <prajwalg@testvagrant.com>
2021-11-12 10:08:16 +0530

    Wrote empty string test and the method to implement

--- src/main/java/StringCalculator.java
+++ src/main/java/StringCalculator.java
@@ -1,4 +1,9 @@
 public class StringCalculator {
 
-
+    public int Add(String numbers){
+        int totalSum = 0;
+        if(numbers==null)
+            totalSum=0;
+        return totalSum;
+    }
 }
--- src/test/java/StringCalculatorTest.java
+++ src/test/java/StringCalculatorTest.java
@@ -1,3 +1,10 @@
-public class StringCalculatorTest {
+import org.junit.Assert;
+import org.junit.Test;
 
+public class StringCalculatorTest {
+    StringCalculator stringCalculator=new StringCalculator();
+    @Test
+    public void emptyStringTest(){
+        Assert.assertEquals(0,stringCalculator.Add(""));
+    }
 }
